1a0156504da73d021ca77f09532c372d4b2b628e
zaccc <zacccmi7@outlook.com>
2022-05-24 10:44:53 -0400

    plein de modif

--- tp5App/models.py
+++ tp5App/models.py
@@ -8,27 +8,17 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
 
     def __str__(self):
         return self.title
 
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
+    def comments(self):
+        return self.comments
 
 class Comment(models.Model):
     post = models.ForeignKey('tp5App.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
-    approved_comment = models.BooleanField(default=False)
-
-    def approve(self):
-        self.approved_comment = True
-        self.save()
 
     def __str__(self):
         return self.text
--- tp5App/views.py
+++ tp5App/views.py
@@ -62,14 +62,6 @@ def add_comment_to_post(request, pk):
         form = CommentForm()
     return render(request, 'tp5App/add_comment_to_post.html', {'form': form})
 
-
-#@login_required
-def comment_approve(request, pk):
-    comment = get_object_or_404(Comment, pk=pk)
-    comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
-
-
 #@login_required
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
