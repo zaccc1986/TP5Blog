f0d4c7ed2adcc7ec91abf1e5b4261a372c078859
zaccc <zacccmi7@outlook.com>
2022-05-23 22:08:34 -0400

    Update views.py

--- tp5App/views.py
+++ tp5App/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .forms import PostForm
 from .models import Post
